5ea9611b1c4d08b64de1930f870f51b27c7fafe9
JoseAndresHV <joseandreshurtadov@gmail.com>
2023-05-30 16:19:09 -0400

    Add project files.

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using System.Web;
+using System.Text;
+using FloralAppFE.Ecommerce.Models;
+using Newtonsoft.Json;
+
+namespace FloralAppFE.Ecommerce.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.HideLayout = true;
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var apiUrl = "https://floralappapi.azurewebsites.net/api";
+            var httpClient = new HttpClient();
+
+            var requestBody = new { model.Email, model.Password };
+            var json = JsonConvert.SerializeObject(requestBody);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            var response = await httpClient.PostAsync($"{apiUrl}/auth/login", content);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var responseContent = await response.Content.ReadAsStringAsync();
+                var token = JsonConvert.DeserializeObject<ResponseModel<string>>(responseContent)!.Data;
+                TempData["token"] = token;
+                
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ModelState.AddModelError("", "Invalid login attempt");
+                return View(model);
+            }
+        }
+    }
+}
---
+++ Models/ResponseModel.cs
@@ -0,0 +1,9 @@
+﻿namespace FloralAppFE.Ecommerce.Models
+{
+    public class ResponseModel<T>
+    {
+        public bool Success { get; set; }
+        public T? Data { get; set; }
+        public string? Message { get; set; }
+    }
+}
